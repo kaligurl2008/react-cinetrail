5497c46cfd80a73f02c02ea4edbce5d36fd5e98b
KaliGurl <kaligurl2008@gmail.com>
2022-12-07 09:59:27 -0500

    completed movie5 live session coding, need css on MovieDetails and Homepage

--- src/Pages/MovieDetails/MovieDetails.js
+++ src/Pages/MovieDetails/MovieDetails.js
@@ -3,6 +3,7 @@ import {useParams} from 'react-router-dom'
 import './MovieDetails.css'
 import axios from 'axios'
 import ReactPlayer from 'react-player'
+import Rating from '../../Components/Rating/Rating'
 
 
 function MovieDetails() {
@@ -22,8 +23,24 @@ function MovieDetails() {
     //create state to hold video info
     const [videoLink, setVideoLink] = React.useState('');
 
+    //create state to hold movie details
+    const [movie, setMovie] = React.useState({});
+    const [rating, setRating] = React.useState(0);
+
     React.useEffect(
         ()=>{
+            //call api to get movie info
+            axios.get(`${baseUrl}movie/${movieId}?api_key=${apiKey}`)
+            .then(res=>{
+                console.log(res.data)
+                setMovie(res.data)
+                setRating(res.data.vote_average/2)
+            })
+            .catch(err=>console.log(err))
+
+
+
+            //call api to get trailer
             axios.get(`${baseUrl}movie/${movieId}/videos?api_key=${apiKey}`)
             .then(res=>{
                 //console.log(res.data.results);
@@ -39,18 +56,56 @@ function MovieDetails() {
             .catch(err=>console.log(err))
         }, []
     )
+
+
+
   return (
-    <div className='details-container'>
-        <div className='trailer-container'>
-            <ReactPlayer 
-            className="trailer-player"
-            url={`https://www.youtube.com/watch?v=${videoLink}`}
-            width="100%"
-            height="100%"
-            />
+    <div className="details-container">
+        {
+            videoLink ?
+            <div className="trailer-container">
+                <ReactPlayer 
+                    className="trailer-player"
+                    url={`https://www.youtube.com/watch?v=${videoLink}`}
+                    width="100%"
+                    height="100%"  
+                />
+            </div>
+            :
+            <div className="trailer-container-blank"
+            style={
+                {
+                backgroundImage:`url("${imageBaseUrl}/${movie?.backdrop_path}")`,
+                backgroundPosition:"center",
+                backgroundSize:"cover"
+                }}  >
+            <p>No trailers released yet</p>
+            </div>
+        }
+        <div className="info-container">
+            {movie?.original_title}
+            <Rating stars={rating} />
+            <div className="moviedetails-info">
+                <img src={`${imageBaseUrl}/${movie?.poster_path}`} 
+                     className="details-poster" 
+                     alt={movie?.title}/>
+                <div className="moviedetails-right">
+                    <h2>{movie?.tagline}</h2>
+                    <h4>{movie?.overview}</h4>
+                    <h4>Status: <span>{movie?.status}</span></h4>
+                    <h4>Runtime: <span>{movie?.runtime}</span></h4>
+                    <h4>Budget: <span>{movie?.budget}</span></h4>
+                </div>
+
+            </div>
         </div>
+
+
     </div>
+
+
   )
 }
+
 
 export default MovieDetails
